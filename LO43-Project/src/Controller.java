64d0434a90526deeb7a0beb5bc6d51b448d7432d
cheikh35 <hamza.jaffali@laposte.net>
2014-11-25 12:07:13 +0100

    J'ai trouvé une solution pour écouter les changement dans la BAL
    --> utiliser le pattern Observable.
    
    Je poursuit dans la découverte/révision de ce pattern, et j'implémente
    tout ça dès que c'est bon

--- LO43-Project/src/Controller.java
+++ LO43-Project/src/Controller.java
@@ -1,12 +1,17 @@
 
-public class Controller {
+public class Controller implements PassagerListener, CarListener{
 
-	MailBox mBox;
+	MailBox mainBox;
 	
 	String requestInTraitment;
 	
+	// Il faut dès maintenant chercher comment récupérer un changement dans un des attributs de BAL. (listener, ...)
+	
+	// ---> solution : utiliser le pattern Observable , et la classe controller doit implémenter les interfaces qui écoutent chaque changement
+	
+	
 	public Controller () {
-		mBox = new MailBox();
+		mainBox = new MailBox();
 	}
 	
 	public void updateListened() {}
