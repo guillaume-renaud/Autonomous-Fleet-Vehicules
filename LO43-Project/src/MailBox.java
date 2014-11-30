9db02aca1092e3ad6246571b85cc726d0ca153ab
OlivierVillequey <oliviervillequey@gmail.com>
2014-11-30 21:22:26 +0900

    I made some changes in my part

--- LO43-Project/src/MailBox.java
+++ LO43-Project/src/MailBox.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class MailBox {
@@ -15,6 +18,22 @@ public class MailBox {
 		listeners = new ArrayList<MailBoxListener>();
 	}
 	
+	public void FileReader() {
+		String filePath = "request.txt";
+		Scanner scanner;
+		try {
+			scanner = new Scanner(new File(filePath));
+			while (scanner.hasNextLine()) {
+			    this.passengers.add(new Passenger(scanner.nextLine()));
+			 }
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("error");
+			e.printStackTrace();
+		}
+
+	}
 	public void addMailBoxListener (MailBoxListener l)
 	{
 		if (!listeners.contains(l))
--- LO43-Project/src/Passenger.java
+++ LO43-Project/src/Passenger.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 
 public class Passenger implements MailBoxListener {
 	
+	int passengerNumber;
 	Request request;
 	boolean occuped;
 	
 	
 	
-	
+	public Passenger(String s){
+		Scanner scanner;
+		scanner = new Scanner(s);
+		scanner.useDelimiter(Pattern.compile("[ : \n]")); // define the delimiter for parsing the line
+		passengerNumber = scanner.nextInt();
+		System.out.println(passengerNumber);
+		scanner.next();
+		String begin = scanner.next();
+		System.out.println(begin);
+		scanner.next();
+		String end = scanner.next();
+		System.out.println(end);
+		request = new Request(begin, end);
+		scanner.close();
+	}
 	
 	@Override
 	public void onMailBoxUpdated(MailBoxEvent e) {
