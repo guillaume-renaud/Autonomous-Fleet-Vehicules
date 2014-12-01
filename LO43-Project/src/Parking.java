6bda070c9ef48b031a31fd575d5c40cea265d62e
guillaume-renaud <renaudg779@gmail.com>
2014-12-01 12:37:59 +0100

    Test reinstall git eclipse

--- LO43-Project/src/Parking.java
+++ LO43-Project/src/Parking.java
@@ -1,8 +1,10 @@
+import java.util.Queue;
 
 public class Parking {
 
+	Queue<Car> parkingQueue;
 	
 	public Parking () {
-		
+		parkingQueue = new Queue<Car>();
 	}
 }
--- LO43-Project/src/Place.java
+++ LO43-Project/src/Place.java
@@ -5,7 +5,5 @@ public class Place {
 	
 	public Place () {
 		
-	}
-
-	
+	}	
 }
