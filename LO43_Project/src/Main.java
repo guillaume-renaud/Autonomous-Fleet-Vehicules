ba31e2f0b8a5fe1e26ce6716c11d4f2c9505ce56
guillaume-renaud <renaudg779@gmail.com>
2014-12-05 22:32:54 +0100

    Modif Window & Main

--- LO43_Project/src/Main.java
+++ LO43_Project/src/Main.java
@@ -1,13 +1,15 @@
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
 		MailBox mainBox = new MailBox();
 		mainBox.FileReader();
 		Controller controller = new Controller(mainBox);
 		
+		JFrame window = new JFrame();
+		window.setVisible(true);
 		
-		GraphicInterface g = new GraphicInterface();
+		// GraphicInterface g = new GraphicInterface();
 	}
 }
--- LO43_Project/src/Window.java
+++ LO43_Project/src/Window.java
@@ -1,10 +1,8 @@
-import javax.swing.JFrame;
 
 public class Window {
 	
 	public static void main(String[] args) {
-		JFrame window = new JFrame();
-		window.setVisible(true);
+		
 		
 	}
 
