e572114e7d13985dffd5184d35f91b74e1d546c1
guillaume-renaud <renaudg779@gmail.com>
2014-12-06 01:47:13 +0100

    Interface Graphique

--- LO43_Project/src/Window.java
+++ LO43_Project/src/Window.java
@@ -2,14 +2,14 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
-import javax.swing.JPanel;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
 
 public class Window extends JFrame 
 {
-	private static final long serialVersionUID = 1L;
-
 	public Window()
 	{
 		this.setTitle("Autonomous Fleet Vehicules");
@@ -17,25 +17,27 @@ public class Window extends JFrame
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);	// JFrame in the center of the window
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	    this.setContentPane(new Panneau());
-		
+			
+		//JLayeredPane layPan = getLayeredPane();
+		//Panneau panneau = new Panneau();
+	    
+		//layPan.add(middle, new Integer(2));
+		this.setContentPane(new Panneau());
 		this.setVisible(true);
 	}
 	
-	public class Panneau extends JPanel {
-		
-		private static final long serialVersionUID = 1L;
-
+	
+	public class Panneau extends JPanel 
+	{
 		public void paintComponent(Graphics g){
 			try {
 				Image img = ImageIO.read(new File("image/fenetre.png"));
 				//Pour une image de fond sinon g.drawImage(img, 0, 0, this);
 				g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
-				g.fillOval(325, 35, 35, 35);
+				//g.fillOval(325, 35, 35, 35);
 	    	} catch (IOException e) {
 	    		e.printStackTrace();
 		    }                
 		}               
-	}
+	}	
 }
