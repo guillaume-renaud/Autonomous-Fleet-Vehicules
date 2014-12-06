b0c4d3bb90c4798272012098866f1071d088c4a3
cheikh35 <hamza.jaffali@laposte.net>
2014-12-06 01:52:32 +0100

    ^^

--- LO43_Project/src/Window.java
+++ LO43_Project/src/Window.java
@@ -29,11 +29,11 @@ public class Window extends JFrame
 
 		public void paintComponent(Graphics g){
 			try {
-				Image img = ImageIO.read(new File("image/fenetre.png"));
-				//Pour une image de fond sinon g.drawImage(img, 0, 0, this);
-				g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
-				g.fillOval(325, 35, 35, 35);
-	    	} catch (IOException e) {
+			Image img = ImageIO.read(new File("image/fenetre.png"));
+			//Pour une image de fond sinon g.drawImage(img, 0, 0, this);
+			g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+			//g.fillOval(325, 35, 35, 35);
+			}catch (IOException e) {
 	    		e.printStackTrace();
 		    }                
 		}               
