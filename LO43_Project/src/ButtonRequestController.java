a6b6659ebf5015dd373ab56eb4b3486fea56b075
cheikh35 <hamza.jaffali@laposte.net>
2014-12-11 18:51:35 +0100

    fenetre de requette

--- LO43_Project/src/ButtonRequestController.java
+++ LO43_Project/src/ButtonRequestController.java
@@ -1,7 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import Window.NewRequest;
 
 
 public class ButtonRequestController implements ActionListener {
--- LO43_Project/src/RequestFrame.java
+++ LO43_Project/src/RequestFrame.java
@@ -1,21 +1,55 @@
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 
 public class RequestFrame extends JFrame {
 
 			private static final long serialVersionUID = 1L;
 			
+			String[] input = {"I1","I2","I3","I4","I5","I6"};
+			String[] output = {"O1","O2","O3","O4","O5","O6"};
+			
+			JPanel customChoice = new JPanel();
+			
+			JComboBox<String> start ;
+			JComboBox<String> end ;
+			
+			JLabel intro;
+			JLabel starting;
+			JLabel ending;
+			
+			JButton ok;
+			JButton cancel;
+			
 			
 			
 			public RequestFrame () {
+				
 				super();
+				
+				customChoice = new JPanel();
+				
+				start = new JComboBox<String>(input);
+				end = new JComboBox<String>(output);
+				
+				ok = new JButton("Ok");
+				cancel = new JButton("Cancel");
+				
+				
+				intro = new JLabel("Please choose your dificulty level");
+				starting = new JLabel("Height (6-24)");
+				ending = new JLabel("Height (6-24)");
 				
 				this.setTitle("New Request");
 				this.setSize(350, 350);
 				this.setResizable(false);
 				this.setAlwaysOnTop(true);
 				this.setLocationRelativeTo(null); // JFrame in the center of the window
-				this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			}
 			
 		}
