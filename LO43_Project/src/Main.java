65ba546f22f07a8be3157c055fe2b402bbedd2c3
Guillaume <Guillaume@Guillaume-PC>
2014-12-12 18:18:02 +0100

    Le main est dans la Place ! (reciproquement proportionnel ou egal)

--- LO43_Project/src/Main.java
+++ LO43_Project/src/Main.java
@@ -1,9 +1,78 @@
+import java.util.ArrayList;
+
 
 public class Main {
 
 	public static void main(String[] args) {
+		int i, j;
 		MailBox mainBox = new MailBox();
 		mainBox.FileReader();
+		
+		//Places declaration and add them into mainBox.reservations
+		mainBox.reservations.add(new Place("C", true, new ArrayList<Place>(),378,287));
+		mainBox.reservations.add(new Place("I1", true, new ArrayList<Place>(),600,90));
+		mainBox.reservations.add(new Place("I2", true, new ArrayList<Place>(),310,17));
+		mainBox.reservations.add(new Place("I3", true, new ArrayList<Place>(),103,170));
+		mainBox.reservations.add(new Place("I4", true, new ArrayList<Place>(),162,480));
+		mainBox.reservations.add(new Place("I5", true, new ArrayList<Place>(),437,550));
+		mainBox.reservations.add(new Place("I6", true, new ArrayList<Place>(),660,390));
+		mainBox.reservations.add(new Place("O1", true, new ArrayList<Place>(),660,177));
+		mainBox.reservations.add(new Place("O2", true, new ArrayList<Place>(),437,17));
+		mainBox.reservations.add(new Place("O3", true, new ArrayList<Place>(),162,90));
+		mainBox.reservations.add(new Place("O4", true, new ArrayList<Place>(),103,390));
+		mainBox.reservations.add(new Place("O5", true, new ArrayList<Place>(),310,550));
+		mainBox.reservations.add(new Place("O6", true, new ArrayList<Place>(),600,480));
+		mainBox.reservations.add(new Place("R1", true, new ArrayList<Place>(),563,180));
+		mainBox.reservations.add(new Place("R2", true, new ArrayList<Place>(),377,90));
+		mainBox.reservations.add(new Place("R3", true, new ArrayList<Place>(),192,170));
+		mainBox.reservations.add(new Place("R4", true, new ArrayList<Place>(),206,390));
+		mainBox.reservations.add(new Place("R5", true, new ArrayList<Place>(),378,483));
+		mainBox.reservations.add(new Place("R6", true, new ArrayList<Place>(),563,390));
+		
+		//Fill in the adjacencyListPlace for each place
+		for(i=1; i<7; i++)
+		{
+			mainBox.findSpecificPlace("C").getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R"+i));
+			mainBox.findSpecificPlace("R"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("C"));
+		}
+		for(i=1; i<7; i++)
+		{
+			mainBox.findSpecificPlace("I"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R"+i));
+			mainBox.findSpecificPlace("O"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R"+i));
+		}		
+		for(i=1; i<7; i++)
+		{
+			mainBox.findSpecificPlace("R"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("O"+i));
+			if(i==1)
+			{
+				mainBox.findSpecificPlace("R"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R6"));
+				mainBox.findSpecificPlace("R"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R2"));
+			}
+			else if(i==6)
+			{
+				mainBox.findSpecificPlace("R"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R5"));
+				mainBox.findSpecificPlace("R"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R1"));
+			}
+			else
+			{
+				j = i+1;
+				mainBox.findSpecificPlace("R"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R"+j));
+				j = i-1;
+				mainBox.findSpecificPlace("R"+i).getAdjacencyPlaceList().add(mainBox.findSpecificPlace("R"+j));
+			}
+		}
+
+		for( Place p : mainBox.reservations)
+		{
+			System.out.println(p.getPlaceName() + " :");
+			
+			for (Place pl : p.getAdjacencyPlaceList()) 
+				System.out.print(pl.getPlaceName() + " ");
+
+			System.out.println();
+		}
+		
+		
 		
 		//test instantiation multiple cars
 		mainBox.fleet.add(new Car(1, "P1", 563 ,180));
@@ -12,8 +81,6 @@ public class Main {
 		//mainBox.fleet.add(new Car(4, "P4", 246 ,375));
 		//mainBox.fleet.add(new Car(5, "P5", 384 ,471));
 		//mainBox.fleet.add(new Car(6, "P6", 534, 385));
-		
-		
 		
 		Window window = new Window(mainBox);
 		mainBox.addMailBoxListener(window);
--- LO43_Project/src/Place.java
+++ LO43_Project/src/Place.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+ import java.util.ArrayList;
 
 
 public class Place 
