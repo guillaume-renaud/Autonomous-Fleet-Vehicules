569fbbf70cbd182548bb5a527c74de903256e3e6
OlivierVillequey <oliviervillequey@gmail.com>
2014-12-28 03:55:37 +0900

    requestMap

--- LO43_Project/src/Request.java
+++ LO43_Project/src/Request.java
@@ -11,9 +11,17 @@ public class Request {
 	boolean[] requestMap;
 	String[] requestMapPlaceName;
 	
+<<<<<<< HEAD
 public Request(String begin, String end){
 	int index = 0;
 	requestMapPlaceName = new String[] {"I1","I2","I3","I4","I5","R1","R2","R3","R4","R5","R6","O1","O2","O3","O4","O5","O6","C"};
+=======
+public Request(String begin, String end){
+	int index = 0;
+
+	requestMapPlaceName = new String[] {"I1","I2","I3","I4","I5","I6","R1","R2","R3","R4","R5","R6","O1","O2","O3","O4","O5","O6","C"};
+
+>>>>>>> branch 'master' of https://github.com/guillaume-renaud/Autonomous-Fleet-Vehicules.git
 	String filePath = "requestMap.txt";
 	Scanner sc;
 	try {
