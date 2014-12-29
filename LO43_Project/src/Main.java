6ff06a4924293fe8f9edc519b28e99bb5d45eeed
cheikh35 <hamza.jaffali@laposte.net>
2014-12-29 13:27:43 +0100

    Je fais bouger car1 de I1 à O2 et car2 de I3 à O4 !!! Sauf kon voi rien

--- LO43_Project/src/Main.java
+++ LO43_Project/src/Main.java
@@ -84,25 +84,26 @@ public class Main {
 		
 		
 		//Manual creation of tasks
-		//MailBoxEvent e1 = new MailBoxEvent("Car", 0, "POSITION_CHANGED", null, mainBox.findSpecificPlace("R5"));
-		//MailBoxEvent e2 = new MailBoxEvent("Car", 0, "POSITION_CHANGED", mainBox.findSpecificPlace("R5"), mainBox.findSpecificPlace("O5"));
-		
-		//MailBoxEvent e3 = new MailBoxEvent("Car", 1, "POSITION_CHANGED", null, mainBox.findSpecificPlace("R6"));
-		//MailBoxEvent e4 = new MailBoxEvent("Car", 1, "POSITION_CHANGED", mainBox.findSpecificPlace("R6"), mainBox.findSpecificPlace("O6"));
-		//MailBoxEvent e3 = new MailBoxEvent("Car", 2, "POSITION_CHANGED", null, mainBox.findSpecificPlace("I3"));
-		//MailBoxEvent e4 = new MailBoxEvent("Car", 3, "POSITION_CHANGED", null, mainBox.findSpecificPlace("I4"));
-		//MailBoxEvent e5 = new MailBoxEvent("Car", 4, "POSITION_CHANGED", null, mainBox.findSpecificPlace("I5"));
-		//MailBoxEvent e6 = new MailBoxEvent("Car", 5, "POSITION_CHANGED", null, mainBox.findSpecificPlace("I6"));
+		MailBoxEvent e1 = new MailBoxEvent("Car", 0, "POSITION_CHANGED", null, mainBox.findSpecificPlace("I1"));
+		MailBoxEvent e2 = new MailBoxEvent("Car", 0, "POSITION_CHANGED", mainBox.findSpecificPlace("I1"), mainBox.findSpecificPlace("R1"));
+		MailBoxEvent e3 = new MailBoxEvent("Car", 0, "POSITION_CHANGED", mainBox.findSpecificPlace("R1"), mainBox.findSpecificPlace("R2"));
+		MailBoxEvent e4 = new MailBoxEvent("Car", 0, "POSITION_CHANGED", mainBox.findSpecificPlace("R2"), mainBox.findSpecificPlace("O2"));
 		
 		
-		//window.tasks.addLast(e1);
-		//window.tasks.addLast(e2);
-		//window.tasks.addLast(e3);
-		//window.tasks.addLast(e4);
-		///window.tasks.addLast(e4);
-		//window.tasks.addLast(e5);
-		//window.tasks.addLast(e6);
-		//window.tasks.addLast(e7);
+		MailBoxEvent e5 = new MailBoxEvent("Car", 1, "POSITION_CHANGED", null, mainBox.findSpecificPlace("I3"));
+		MailBoxEvent e6 = new MailBoxEvent("Car", 1, "POSITION_CHANGED", mainBox.findSpecificPlace("I3"), mainBox.findSpecificPlace("R3"));
+		MailBoxEvent e7 = new MailBoxEvent("Car", 1, "POSITION_CHANGED", mainBox.findSpecificPlace("R3"), mainBox.findSpecificPlace("R4"));
+		MailBoxEvent e8 = new MailBoxEvent("Car", 1, "POSITION_CHANGED", mainBox.findSpecificPlace("R4"), mainBox.findSpecificPlace("O4"));
+
+		
+		window.tasks.addLast(e1);
+		window.tasks.addLast(e2);
+		window.tasks.addLast(e3);
+		window.tasks.addLast(e4);
+		window.tasks.addLast(e5);
+		window.tasks.addLast(e6);
+		window.tasks.addLast(e7);
+		window.tasks.addLast(e8);
 		
 		Thread affichage = new Thread(window);
 		
@@ -118,7 +119,7 @@ public class Main {
 		
 		//window.moveToStartingPoint(window.mainBox.findSpecificPlace("I1"), mainBox.fleet.get(0));
 		//window.moveCarView(window.mainBox.findSpecificPlace("I1"), window.mainBox.findSpecificPlace("R1"),mainBox.fleet.get(0));
-		mainBox.commandControl.start();
+		//mainBox.commandControl.start();
 
 		
 		//System.out.println("nombre d'events dans tasks de Window : "+window.tasks.size());
