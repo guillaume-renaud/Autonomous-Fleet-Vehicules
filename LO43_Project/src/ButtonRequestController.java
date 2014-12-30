3e1f2e257b4c537308330f2d765c495c953a3107
cheikh35 <hamza.jaffali@laposte.net>
2014-12-30 17:25:54 +0100

    J'ai fini de coder les boutons New Request et Random Request, mais il faut voir pk on fire pas bien ! Olivier ... sors de corps !

--- LO43_Project/src/ButtonRequestController.java
+++ LO43_Project/src/ButtonRequestController.java
@@ -1,5 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 
 
@@ -22,7 +23,26 @@ public class ButtonRequestController implements ActionListener {
 				}
 				else if (button.getText().equals("Random Request"))
 				{
+					int depart;
+					Random rand = new Random();
+					int arrivee =0;
+					 
+					depart = rand.nextInt(6 - 1 + 1) + 1;
+					while(arrivee==0 || arrivee==depart)
+					{
+						arrivee= rand.nextInt(6 - 1 + 1) + 1;
+					}
 					
+					Passenger p = new Passenger((button.rf.mainBox.passengers.size()+1)+" start:I"+depart+" destination:O"+arrivee, button.rf.mainBox);
+					
+					button.rf.mainBox.passengers.addLast(p);
+					button.rf.mainBox.addMailBoxListener(p);
+				
+					MailBoxEvent event = new MailBoxEvent (p.getClass().getName(), button.rf.mainBox.passengers.indexOf(p), "NEW_REQUEST");
+					
+					System.out.println("Requette envoyée");
+					
+					button.rf.mainBox.fireMailBoxUpdated(event);
 				}
 				
 				
