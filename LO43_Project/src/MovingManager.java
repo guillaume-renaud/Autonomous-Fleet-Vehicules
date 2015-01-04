adbaa7cca612809b85869831bf95ba10bcef06e3
OlivierVillequey <oliviervillequey@gmail.com>
2015-01-05 06:12:14 +0900

    comment MovingManager

--- LO43_Project/src/MovingManager.java
+++ LO43_Project/src/MovingManager.java
@@ -6,13 +6,13 @@ public class MovingManager extends Thread {
 			String name;
 			boolean isRunning = false;
 			
-			
+			// to initialize the internalThread
 			public MovingManager(String name, Window w)
 			{
 				this.name = name;
 				this.window = w;
 			}
-			
+			//to set an event to the Thread
 			public void setManageredObjects (MailBoxEvent e, Window w, String name)
 			{
 				actualManagedEvent = e;
@@ -20,7 +20,7 @@ public class MovingManager extends Thread {
 				actualManagedCar = window.mainBox.fleet.get(e.indexUpdaterInMailBoxList);
 				this.name = name;
 			}
-			
+			// this is a flag to help window knowing when to stop execution and start Calcul() again.
 			public boolean isRunning()
 			{
 				return isRunning;
@@ -39,20 +39,24 @@ public class MovingManager extends Thread {
 						isRunning = true;
 
 						window.nbFreeThread--;
-
+						//First, we check if the action is POSITION_CHANGED or PARKED
 						if (actualManagedEvent.updateAction.equals("POSITION_CHANGED"))
 						{
+							// case of enroll of a car
 							if (actualManagedEvent.lastPlace==null)
 							{
 								actualManagedCar.setDisplayed(true);
 								window.moveToStartingPoint(window.mainBox.findSpecificPlace(actualManagedEvent.newPlace.getPlaceName()),actualManagedCar);
 							}
+							//simple move from a place to another
 							else
 							{
 								window.moveCarView(window.mainBox.findSpecificPlace(actualManagedEvent.lastPlace.getPlaceName()), window.mainBox.findSpecificPlace(actualManagedEvent.newPlace.getPlaceName()),actualManagedCar);
 							}
 
 						}
+						/*if it's PARKED event then we move the car to the parking and make 
+						 * it disappear from the screen.*/
 						else if (actualManagedEvent.updateAction.equals("PARKED"))
 						{
 							window.moveToParking(actualManagedCar);
@@ -60,6 +64,7 @@ public class MovingManager extends Thread {
 							actualManagedCar = null;
 
 						}
+						//we change the flag value for next call
 						window.nbFreeThread++;
 						isRunning = false;
 						
@@ -68,7 +73,7 @@ public class MovingManager extends Thread {
 					}	
 					
 			}
-			
+			// we used sleep() and interrupt() to synchronize the tasks.
 			private void sleep(int second)
 			{
 				try {
